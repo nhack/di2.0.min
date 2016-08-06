5673e234311c345281b973eb74b12dd5c669d2fd
Marius Cristea <marius.cristea@irian.ro>
2016-08-06 21:15:11 +0200

    S02 - Dependency Injection Container

--- app/pizza/pizza.component.ts
+++ app/pizza/pizza.component.ts
@@ -6,14 +6,14 @@ import { PizzaFileService } from './service/pizzaFile.service';
 @Component({
   selector: 'pizza',
   templateUrl: 'app/pizza/pizza.component.html',
-  styleUrls: ['app/pizza/pizza.component.css']
+  styleUrls: ['app/pizza/pizza.component.css'],
+  providers: [PizzaFileService]
 })
 export class PizzaComponent {
 
   private pizzas: Pizza[];
-  private pizzaService: PizzaFileService = new PizzaFileService();
 
-  constructor() {
+  constructor(private pizzaService: PizzaFileService) {
     this.pizzas = this.pizzaService.getPizzas();
   }
 }
--- app/pizza/service/pizzaFile.service.ts
+++ app/pizza/service/pizzaFile.service.ts
@@ -1,6 +1,9 @@
+import {Injectable} from '@angular/core';
+
 import { Pizza } from '../domain/pizza';
 import { PIZZAS } from '../domain/data';
 
+@Injectable()
 export class PizzaFileService {
   getPizzas(): Pizza[] {
     return PIZZAS
